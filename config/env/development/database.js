7b1d035ab50e33abf904b7396fcd1b73fc69f9c9
Etienne-bobo <etiennebobo17@gmail.com>
2021-06-16 20:14:16 +0530

    deploy to heruko

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -4,16 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME', 'stepcoding-cms'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+        uri: env('DATABASE_URI'),
+        port: env.int('DATABASE_PORT', 27017),
+        database: 'stepCoding2',
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -6,9 +6,11 @@ module.exports = ({ env }) => ({
       settings: {
         srv: true,
         uri: env('DATABASE_URI'),
+        port: env.int('DATABASE_PORT', 27017),
+        database: 'stepCoding-server',
       },
       options: {
-        authenticationDatabase: '',
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
         ssl: true,
       },
     },
