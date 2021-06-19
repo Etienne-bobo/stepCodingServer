c37d6b05d8c6b9ae56965093cf850f3e059a7bd7
etienne.blavo <etiennebobo17@gmail.com>
2021-06-19 18:31:39 +0530

    mail

--- config/env/development/plugins.js
+++ config/env/development/plugins.js
@@ -9,4 +9,16 @@ module.exports = ({ env }) => ({
     },
   },
   // ...
+  email: {
+    provider: 'mailgun',
+    providerOptions: {
+      apiKey: env('MAILGUN_API_KEY'),
+      //domain: env('MAILGUN_DOMAIN'), //Required if you have an account with multiple domains
+      host: env('MAILGUN_HOST', 'api.mailgun.net'), //Optional. If domain region is Europe use 'api.eu.mailgun.net'
+    },
+    settings: {
+      defaultFrom: 'etiennebobo17@gmail.com',
+      defaultReplyTo: 'etiennebobo17@gmail.com',
+    },
+  },
 });
